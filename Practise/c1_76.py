968ca925769c630b380e04a3b412bd5682a048f0
Nirmal <nirmalvp1502@gmail.com>
2015-05-10 23:16:29 +0530

    Added Till c1_77

--- Practise/c1_76.py
+++ Practise/c1_76.py
@@ -1,4 +1,4 @@
-#76A) Given a collection of numbers, return all possible combinations
+# -*- coding: utf-8 -*
 def findNcK(numbers,k):
 	for index,number in enumerate(numbers):
 		if k == 1 :
@@ -11,7 +11,41 @@ def allPossibleCombinations(numbers):
 	for k in range(1,len(numbers)+1):
 		print list(findNcK(numbers,k))
 
+def addToSum(numbers,target,path):
+	if target < 0 :
+		return 
+	if target == 0 :
+		print path
+		return
+	for index,number in enumerate(numbers):
+		newTarget = target-number
+		path.append(number)
+		addToSum(numbers[index:],newTarget,path[:])
+		path.pop()
+
 def main():
+	#76A) Given a collection of numbers, return all possible combinations
 	numbers = map(int,raw_input("Enter the numbers").split())
 	allPossibleCombinations(numbers)
+	
+	#76C) Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C 
+	#where the candidate numbers sums to T. 
+	#The same repeated number may be chosen from C unlimited number of times. 
+	#All numbers (including target) will be positive integers.  
+	#Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak). 
+	#The solution set must not contain duplicate combinations.  
+	#For example, given candidate set 2,3,6,7 and target 7, A solution set is: [7], [2, 2, 3]
+
+	numbers = map(int,raw_input("Enter the numbers").split())
+	target = input("Enter the target Sum")
+	addToSum(numbers,target,[])
+
 main()
+
+
+
+
+
+
+
+
---
+++ Practise/c1_77.py
@@ -0,0 +1,20 @@
+#77) The gray code is a binary numeral system where two successive values differ in only one bit.   [LeetCode]    
+#        Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.        
+#        For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:  00 - 0  01 - 1  11 - 3  10 - 2 
+def printGreyCode(bits):
+	if bits == 1:
+		return [0,1]
+	prevGreyCode = printGreyCode(bits-1)
+	greyCode = list(prevGreyCode)
+	# When bits =2 greyCode = [0,1] now append 1 to left of each element in greyCode reversed to get [0,1,11,10]. 
+	#using shifted to attain proper bit position of 1
+	shifted = (1<<(bits-1))
+	for number in reversed(prevGreyCode):
+		greyCode.append( shifted | number )
+	return greyCode
+
+def main():
+	print printGreyCode(input("Enter Number of bits"))
+
+main()
+
